12e00b91a28e50e6ce7c89e54c23a64113974072
沈剑心 <96043237+faithYaSuo@users.noreply.github.com>
2026-01-28 22:48:33 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -478,6 +478,7 @@ export default {
           "sub.cm": "https://sub.cm/short",
         },
         customBackend: {
+          "默认后端": "https://subc.acbcc.cc"
           "肥羊增强型后端【vless reality+anytls】": "https://api.v1.mk",
           "肥羊备用后端【vless reality+anytls】": "https://url.v1.mk",
         },
@@ -1416,6 +1417,7 @@ export default {
   }
 };
 </script>
+
 
 
 
